e6ac2e19767a30cce9c1d74ae235942f75dc19ea
Swati Gupta <73897489+swatigupta121@users.noreply.github.com>
2021-10-10 22:21:59 +0530

    Update Ques_6.cpp

--- Milestone-1/Ques_6.cpp
+++ Milestone-1/Ques_6.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int,int> m;
-        for(int i=0;i<nums.size();i++)
-            m[nums[i]]=m[nums[i]]+1;
-        for(int i=0;i<nums.size();i++)
-            if(m[nums[i]]>nums.size()/2)
-                return nums[i];
-        return -1;
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()/2];
     }
 };
