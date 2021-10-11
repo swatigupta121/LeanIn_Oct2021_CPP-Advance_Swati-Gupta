b6fa1ade6ed92d60e099b0a7d77d84ec3deb5305
Swati Gupta <73897489+swatigupta121@users.noreply.github.com>
2021-10-11 23:28:54 +0530

    Create Ques_12.cpp

---
+++ Milestone-1/Ques_12.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    // Function to return the count of the number of elements in
+    // the intersection of two arrays.
+    int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
+        // Your code goes here
+      
+        int count=0;
+        set<int> s;
+ 
+        for (int i = 0; i < n; i++)
+            s.insert(a[i]);
+     
+        for (int i = 0; i < m; i++)
+            if (s.find(b[i]) != s.end())
+                count++;
+        return count;
+    }
+};
